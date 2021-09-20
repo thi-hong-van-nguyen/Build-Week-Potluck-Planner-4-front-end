5901fb6bf3b6b03c07e4d7122c789284d74eff6b
ehsan2793 <ehsan2793@gmail.com>
2021-09-20 16:05:58 -0700

    login page form added and functional need to enter the server url

--- potluck/src/components/Login.js
+++ potluck/src/components/Login.js
@@ -1,7 +1,70 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom'
 
-export default function Login() {
+
+const initialFormState = {
+    username: "",
+    password: "",
+};
+
+const Login = () => {
+    const { push } = useHistory()
+    const [formState, setFormState] = useState(initialFormState);
+    const [error, setError] = useState('')
+    const inputChange = e => {
+        const { name, value } = e.target;
+        setFormState({ ...formState, [name]: value });
+
+    }
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        axios.post('url', formState)      // we neeed to enter actual ural here form back end 
+            .then(response => {
+                console.log(response)
+                localStorage.setItem('token',/** whatever is in the response */)
+                push(/*push to whatever page we wwant to go most likey protected page*/)
+
+            })
+            .catch(err => {
+                setError('there is a propblem with the server please try again')
+            })
+
+    }
+
     return (
-        <div></div>
+        <>
+            <form onSubmit={handleLogin} className='login-form' >
+
+                <label className='login-label' htmlFor="username">
+                    Name:
+                    <input className="login-input"
+                        id="username"
+                        type="text"
+                        name="username"
+                        onChange={inputChange}
+                        value={formState.username}
+                    />
+                </label>
+                <label className='login-label' htmlFor="password">
+                    Password:
+                    <input className="login-input"
+                        id="password"
+                        type="text"
+                        name="password"
+                        onChange={inputChange}
+                        value={formState.password}
+                    />
+                </label>
+
+
+                <button className={'login-button'}>Login</button>
+            </form>
+            {error ? <p style={{ color: 'red' }}>{error}</p> : null}
+
+
+        </>
     )
 }
+export default Login
