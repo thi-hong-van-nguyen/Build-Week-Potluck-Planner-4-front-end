bb3ef17cfd7b3cf5b6685ecf4d3303dfa11aa103
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-09-24 14:22:49 -0400

    added detaila and ability to go to potluck

--- potluck/src/components/Potluck.js
+++ potluck/src/components/Potluck.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Link } from "react-router-dom"
 
 export default function Potluck(props) {
-    const { potluck_name, date, time, location} = props.potluck;
+    const { potluck_name, date, time, location, potluck_id } = props.potluck;
     return (
-        <div >
+        <div>
             <div>Potluck name: {potluck_name}</div>
             <div>Date: {date}</div>
             <div>Time: {time}</div>
             <div>Location: {location}</div>
+            <Link to={`/potlucks/${potluck_id}`}>More Details</Link>
         </div>
     )
 }
